ba8a85296e2d46cd9279e1ece03bdeec2a45d41d
Velina Genova <viligen@gmail.com>
2022-10-14 16:49:21 +0200

    making smarter all fields 'disabled'/'readonly'

--- examproject/exam/forms.py
+++ examproject/exam/forms.py
@@ -105,3 +105,11 @@ class DeleteItemForm(forms.ModelForm):
                 })
 
         }
+
+
+# class DeleteAlbumForm(CreateAlbumForm):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         for (_, field) in self.fields.items():
+#             field.widget.attrs['disabled'] = 'disabled'
+#             field.widget.attrs['readonly'] = 'readonly'
